d9121d362bb8b76edade2b8cabdea29ea014f5fe
Ubuntu <azureuser@vm1.lefznslrt1tufhnn3qpvgqcy2h.bx.internal.cloudapp.net>
2021-12-30 23:39:40 +0000

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+enum TileKind {
+    Red,
+    Yellow,
+    Blue,
+    Black,
+    White,
+    BLANK, 
+}
+
+struct Board {
+    quilt: Vec<TileKind>,
+    working: Vec<TileKind>
+}
+
+impl Board {
+
+}
+
+
+
+fn main() {
+    let mut board1 = Board {
+        quilt
+    }
+}
